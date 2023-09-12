111d9b779600c8c3587029dbd386649d803ab296
kai ling <keviniling@Kevins-MacBook-Pro.local>
2023-09-12 23:52:32 +0800

    add template

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "INFO : DAQ-refine started"
+
+cd "/bio/kihara-web/www/em/emweb-jobscheduler/algorithms/DAQ-Refine"
+#source /etc/profile.d/modules.sh
+module load miniconda38
+
+#export CUDA_DEVICE_ORDER="PCI_BUS_ID"
+#export CUDA_VISIBLE_DEVICES=1
+#Inputs
+map=$1
+structure=$2
+output_dir=$3
+eval "$(conda shell.bash hook)"
+# Acitivate the conda enviroment
+module load cryoread
+
+python3 main.py --mode=0 -F=$map -P=$structure --output=$output_dir --window 9 --stride 2 --batch_size=64
+python3 writejobyml.py $output_dir
+
+echo "INFO : DAQ-refine Done"
