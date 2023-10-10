881e5ef4229b6f6205db2c8ec225185ab920a176
kai ling <keviniling@Kevins-MacBook-Pro.local>
2023-10-10 21:36:39 +0800

    query sequence

--- writejobyml.py
+++ writejobyml.py
@@ -26,7 +26,7 @@ data=data[data>0]
 sort_data=np.sort(data)
 contour=float(sort_data[int(0.05*len(sort_data))])
 with open(f'{parent_folder}/job.yml', 'w') as outfile:
-    outfile.write("pdbfiles: daq_score_w9.pdb\n")
+    outfile.write("pdbfiles: daq_score_w9_reverse.pdb\n")
     outfile.write("contour: %f\n"%contour)
     outfile.write("strings: DAQ-refine is a protocol using <a href='https://em.kiharalab.org/algorithm/daqscore'>DAQ score</a> to evaluate protein models from cryo-EM maps and employs a modified AlphaFold 2 for refining regions with potential errors.<br> The 3D model is colored by <a href='https://www.nature.com/articles/s41592-022-01574-4'>DAQ(AA) score</a> scaled from red (-1.0) to blue (1.0) with a 19 residues sliding window. Here blue indicates good score, while red indicates bad score from DAQ. <br> If you encounter any questions for the scored structure, feel free to email dkihara@purdue.edu, gterashi@purdue.edu and wang3702@purdue.edu.\n")
     #yaml.dump(data, outfile, default_flow_style=False)
