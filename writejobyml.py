a263c8630cc00ee503d14be6ed5477e8b1ce81b9
kai ling <keviniling@Kevins-MacBook-Pro.local>
2023-10-25 01:18:08 +0800

    rosetta relaxation

--- writejobyml.py
+++ writejobyml.py
@@ -124,8 +124,9 @@ with mrcfile.open(map_path,permissive=True) as mrc:
 data=data[data>0]
 sort_data=np.sort(data)
 contour=float(sort_data[int(0.05*len(sort_data))])
+cleaned_directory = highest_directory.replace(output_folder, '', 1)
 with open(f'{job_folder}/job.yml', 'w') as outfile:
-    outfile.write("pdbfiles: %s/daq_score_w9_reverse.pdb\n"%highest_directory)
+    outfile.write("pdbfiles: %s/daq_score_w9_reverse.pdb\n"%cleaned_directory)
     outfile.write("contour: %f\n"%contour)
     outfile.write("strings: DAQ-refine is a protocol using <a href='https://em.kiharalab.org/algorithm/daqscore'>DAQ score</a> to evaluate protein models from cryo-EM maps and employs a modified AlphaFold 2 for refining regions with potential errors.<br> The 3D model is colored by <a href='https://www.nature.com/articles/s41592-022-01574-4'>DAQ(AA) score</a> scaled from red (-1.0) to blue (1.0) with a 19 residues sliding window. Here blue indicates good score, while red indicates bad score from DAQ. <br> If you encounter any questions for the scored structure, feel free to email dkihara@purdue.edu, gterashi@purdue.edu and wang3702@purdue.edu.\n")
     #yaml.dump(data, outfile, default_flow_style=False)
