be33d9394405219ecc56840778af5d2e25ed29b9
kai ling <keviniling@Kevins-MacBook-Pro.local>
2023-11-15 17:34:26 +0800

    error handling

---
+++ run_daq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "DAQ Started"
+
+cd "/bio/kihara-web/www/em/emweb-jobscheduler/algorithms/DAQ"
+#source /etc/profile.d/modules.sh
+module load miniconda38
+#
+#export CUDA_DEVICE_ORDER="PCI_BUS_ID"
+#export CUDA_VISIBLE_DEVICES=1
+#Inputs
+map=$1
+structure=$2
+output_dir=$3
+echo "$3"
+eval "$(conda shell.bash hook)"
+# Acitivate the conda enviroment
+module load cryoread
+which python3
+python3 main.py --mode=0 -F=$map -P=$structure --output=$output_dir --window 9 --stride 2 --batch_size=64
