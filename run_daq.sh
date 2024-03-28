b9e149fc3cc0c51009dd93afeb9c762089372e27
kai ling <keviniling@Kevins-MacBook-Pro.local>
2024-03-27 22:25:10 -0400

    debug

--- run_daq.sh
+++ run_daq.sh
@@ -8,13 +8,13 @@ echo "DAQ Started"
 
 cd "$emweb_path/DAQ"
 #source /etc/profile.d/modules.sh
-module load miniconda38
+# module load miniconda38
 #
 #export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 #export CUDA_VISIBLE_DEVICES=1
 eval "$(conda shell.bash hook)"
 # Acitivate the conda enviroment
-module load cryoread
+# module load cryoread
 which python3
 python3 main.py --mode=0 -F=$map -P=$structure --output=$output_dir --window 9 --stride 2 --batch_size=64 --server 1
 python3 $emweb_path/DAQ-Refine/utils/gen_score.py $output_dir
